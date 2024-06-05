fd76c59581a6521d65b1eb4f377a64a05fcf8ce5
Dukemz <mdaakhals@gmail.com>
2024-06-05 15:08:48 +0100

    game hud is now its own sprite

--- js/draw.js
+++ js/draw.js
@@ -4,39 +4,13 @@ function draw() {
   if(document.hidden) {
     window.lastHidden = Date.now();
   }
-  background(color("#24283880"));
+  background(bgcol);
   // background(color("#242838"))
 
   // average deltatime, fps calcs
   avgFPS = fpsList.reduce((a, b) => a + b, 0)/fpsList.length || 0;
   avgDeltaTime = 1/avgFPS;
 
-  noStroke();
-
-  // behold: the texting
-  fill(255);
-  textSize(20);
-  // top left HUD
-  textAlign(LEFT, TOP);
-  text(`frames: ${frameCount}`, 10, 10);
-  text(`realtime: ${world.realTime}`, 10, 40);
-  // right HUD
-  textAlign(RIGHT, TOP);
-  text(`player: ${Math.round(game.player.x)}, ${Math.round(game.player.y)} || mouse: ${Math.round(mouse.x)}, ${Math.round(mouse.y)}`, width-10, 10);
-  text(`bullets fired: ${game.player.weapons[game.player.activeWeapon].bulletsFired}`, width-10, 40);
-  // bottom left HUD
-  noStroke()
-  textAlign(LEFT, BOTTOM);
-  text(`${frameRate().toFixed(0)}fps, avg ${avgFPS.toFixed(0)}`, 10, height-40);
-  text(`deltaTime = ${deltaTime}, avg ${Math.round(avgDeltaTime*1000)}`, 10, height-10);
-
-  stroke(255);
-  strokeWeight(2);
-
-  // debug stuff for showing mouse pos
-  // line(mouse.canvasPos.x, 0, mouse.canvasPos.x, height);
-  // line(0, mouse.canvasPos.y, width, mouse.canvasPos.y);
-  // ellipse(mouse.canvasPos.x, mouse.canvasPos.y, 10)
   game.draw();
 }
 // after this the draw functions of sprites are called
--- js/gamehud.js
+++ js/gamehud.js
@@ -1,13 +1,18 @@
-class HUD extends Sprite {
+class GameHUD extends Sprite {
   constructor() {
     super();
-    this.layer = 999;
-    this.stroke = "none";
-    this.fill = 255;
-    this.textSize = 20;
+    this.collider = "none";
+    this.x = 0;
+    this.y = 0;
+    this.autoDraw = false;
   }
 
   draw() {
+    push();
+    translate(this.x, this.y);
+    noStroke();
+    fill(255);
+    textSize(20);
     // top left HUD
     textAlign(LEFT, TOP);
     text(`frames: ${frameCount}`, 10, 10);
@@ -17,9 +22,10 @@ class HUD extends Sprite {
     text(`player: ${Math.round(game.player.x)}, ${Math.round(game.player.y)} || mouse: ${Math.round(mouse.x)}, ${Math.round(mouse.y)}`, width-10, 10);
     text(`bullets fired: ${game.player.weapons[game.player.activeWeapon].bulletsFired}`, width-10, 40);
     // bottom left HUD
-    noStroke()
     textAlign(LEFT, BOTTOM);
     text(`${frameRate().toFixed(0)}fps, avg ${avgFPS.toFixed(0)}`, 10, height-40);
     text(`deltaTime = ${deltaTime}, avg ${Math.round(avgDeltaTime*1000)}`, 10, height-10);
+
+    pop();
   }
 }
