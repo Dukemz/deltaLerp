4bc97f244d71d31f693548a54517f8c9cea1063f
Dukemz <mdaakhals@gmail.com>
2024-09-10 13:54:23 +0100

    q5 fps

--- js/gamehud.js
+++ js/gamehud.js
@@ -20,8 +20,9 @@ class GameHUD {
     text(`player: ${Math.round(player.x)}, ${Math.round(player.y)} || mouse: ${Math.round(mouse.x)}, ${Math.round(mouse.y)}`, width-10, 10);
     text(`bullets fired: ${player.weapons[player.activeWeapon].bulletsFired}, total ${player.projectiles.amount}`, width-10, 40);
     // bottom left HUD
+    const q5fps = window.Q5 ? `(q5: ${getFPS()})` : "";
     textAlign(LEFT, BOTTOM);
-    text(`${frameRate().toFixed(0)}fps, avg ${manager.avgFPS.toFixed(0)}`, 10, height-40);
+    text(`${frameRate().toFixed(0)}fps, avg ${manager.avgFPS.toFixed(0)} ${q5fps}`, 10, height-40);
     text(`deltaTime = ${deltaTime.toFixed(0)}, avg ${Math.round(manager.avgDeltaTime*1000)}`, 10, height-10);
     // bottom right HUD
     textAlign(RIGHT, BOTTOM);
