9c85317205df871112862faab6ba87d642302625
Dukemz <mdaakhals@gmail.com>
2024-10-21 22:51:32 +0100

    startButton now uses menuNode class
    going to have to look into making the menu look better and stuff

--- tests/oldmenu.js
+++ tests/oldmenu.js
@@ -7,6 +7,7 @@ class Menu {
     this.menuSprites = new Group();
     // this.menuSprites.autoDraw = false;
     this.menuSprites.strokeWeight = 5;
+    this.menuSprites.fill = color(0, 0, 0, 0);
 
     // menu buttons, not including the start button
     this.menuButtons = new this.menuSprites.Group();
@@ -18,59 +19,36 @@ class Menu {
 
         Object.assign(this, data);
 
-        // since sprites with no colliders have no sensors by default,
-        // sensors must be added manually for mouse hover detection
+        // add default sensors for mouse hover detection
         this.addDefaultSensors();
 
         // set fill, stroke and rotation if not specified in data
         this.fill = data.fill ? data.fill : color("#1b1c56");
         this.stroke = data.stroke ? data.stroke : color("#3f48cc");
+        this.rotation = data.rotation ? data.rotation : 45;
 
         // controls scale
-        this.scaleLerp ??= new LerpController(1, 1, 0.9999);
-        // controls rotation
-        this.rotateLerp ??= new LerpController(this.rotation, this.rotation, 0.9999);
-        // controls stroke - optional
-        this.strokeLerp = data.strokeLerp || null;
-
-        // determines whether or not to set the scale target to the values below
-        this.scaleWithDefaultRules = true;
-        // intended scale values for hovering, pressing or default
-        this.hoverScale ||= 1.1;
-        this.pressedScale ||= 0.9;
-        this.defaultScale ||= 1;
+        this.scaleLerp ||= new LerpController(1, 1, 0.9999);
       }
 
       draw() {
         if(!this.visible) return;
-        // if stroke lerp controller exists, use it to set the colour
-        if(this.strokeLerp) this.stroke = this.strokeLerp.updateCol();
-
         // default draw func
         this._display();
 
-        // update rotation
-        if(this.rotateLerp) this.rotation = this.rotateLerp.update();
-
-        if(this.scaleWithDefaultRules) {
-          // change scale based on mouse hovering/pressing
-          if(this.mouse.hovering()) {
-            if(this.strokeLerp) this.strokeLerp.targetValue = 1;
-
-            if(this.mouse.pressing()) {
-              this.scaleLerp.targetValue = this.pressedScale;
-            } else { // hovering, but not pressing
-              this.scaleLerp.targetValue = this.hoverScale;
-            }
-          } else {
-            if(this.strokeLerp) this.strokeLerp.targetValue = 0;
-            this.scaleLerp.targetValue = this.defaultScale;
+        if(this.mouse.hovering()) {
+          if(this.mouse.pressing()) {
+            this.scaleLerp.targetValue = 0.9;
+          } else { // hovering, but not pressing
+            this.scaleLerp.targetValue = 1.1;
           }
+        } else {
+          this.scaleLerp.targetValue = 1;
         }
 
         const sc = this.scaleLerp.update();
         this.scale = { x: sc, y: sc };
-        
+
         // draw shape that goes on top
         if(this.icondraw) {
           push();
@@ -83,8 +61,7 @@ class Menu {
           pop();
         }
       }
-    } // end of menunode class
-
+    }
     console.log("[MENU] Menu instance created.");
   }
 
@@ -99,25 +76,25 @@ class Menu {
     this.bgcol = new ColLerpController(color("#000000"), color("#242838"), 0, 0, 0.99);
     // menu zoom controller
     this.zoomController = new LerpController(1, 1, 0.9);
-    
+
     // START BUTTON
-    this.startButton = new this.MenuNode([0, 0, 100, 100, 'n'], {
-      fill: color(0,0,0,0),
-      rotation: 0,
-      hoverScale: 1.5,
-      pressedScale: 1.3,
-      strokeLerp: new ColLerpController(color("#242838"), color("#4265fc"), 0, 0, 0.999),
-    });
-    
+    this.startButton = new this.menuSprites.Sprite(0, 0, 100, 100, 'n');
+    // since the start button has no colliders, adding default sensor is required for mouse detection
+    this.startButton.addDefaultSensors();
+    // start button properties
+    this.sbStroke = new ColLerpController(color("#242838"), color("#4265fc"), 0, 0, 0.999);
+    // NOTE: maybe replace this with rotateTo/rotateTowards
+    this.sbRotate = new LerpController(0, 0, 0.9999);
+    this.sbScale = new LerpController(0, 1, 0.9999);
+
     this.menuLogoCentre = new this.MenuNode([0, 0, 180, 180, 'n'], {
       visible: false,
-      rotation: 45,
-      icondraw: function(spr) {
+      icondraw: function (spr) {
         beginShape();
-        vertex(-50, 35); // bottom left
+        vertex(-50, 40); // bottom left
         vertex(0, -70); // top
-        vertex(50, 35); // bottom right
-        vertex(0, -5); // bottom notch
+        vertex(50, 40); // bottom right
+        vertex(0, 0); // bottom notch
         endShape(CLOSE);
       }
     });
@@ -139,7 +116,7 @@ class Menu {
   draw() {
     if(!this.active) return; // menu not initialised yet
 
-    if(camera.x !== 0 || camera.y !== 0) camera.pos = {x:0, y:0};
+    if(camera.x !== 0 || camera.y !== 0) camera.pos = { x: 0, y: 0 };
     camera.on();
     // change bg colour
     background(this.bgcol.updateCol());
@@ -149,10 +126,13 @@ class Menu {
 
 
       if(this.startButton) { // remove the initial start button if it exists
-        const buttoninac = (this.startButton.scaleLerp.targetValue-this.startButton.scaleLerp.currentValue).toFixed(2);
+        const buttoninac = (this.sbScale.targetValue - this.sbScale.currentValue).toFixed(2);
         if(buttoninac == "0.00") {
           this.startButton.remove();
           this.startButton = null;
+          this.sbRotate = null;
+          this.sbScale = null;
+          this.sbStroke = null;
 
           // open the menu - this.mainMenuOpen returns true even if menu buttons and such don't exist yet
           this.openMainMenu();
@@ -166,25 +146,45 @@ class Menu {
       }
 
     } else { // main menu NOT open - start button draw code
-  
+      if(this.startButton.mouse.hovering()) {
+        // if hovering
+        this.sbStroke.targetValue = 1;
+        if(this.startButton.mouse.pressing()) {
+          // if pressing make it smaller to give a button effect sorta
+          this.sbScale.targetValue = 1.3;
+        } else {
+          this.sbScale.targetValue = 1.5;
+        }
+      } else { // not hovering
+        this.sbScale.targetValue = 1;
+        this.sbStroke.targetValue = 0;
+      }
+
       // tilt on hovering/unhovering
       if(this.startButton.mouse.hovers() || this.startButton.mouse.hovered()) {
-        this.startButton.rotateLerp.targetValue += 45;
+        this.sbRotate.targetValue += 45;
       }
 
       // after the start button is clicked
       if(this.startButton.mouse.pressed()) {
-        this.startButton.rotateLerp.targetValue = 0;
+        this.sbRotate.targetValue = 0;
         // set scale to larger out of width or height
-        this.startButton.scaleLerp.targetValue = (Math.max(canvas.h+10, canvas.w+10) / 100);
-        this.startButton.strokeLerp.targetValue = 1;
-        this.startButton.scaleWithDefaultRules = false;
+        this.sbScale.targetValue = (Math.max(canvas.h + 10, canvas.w + 10) / 100);
+        this.sbStroke.targetValue = 1;
 
         // this.openMainMenu();
         this.mainMenuOpen = true;
         this.bgcol.targetValue = 0.8;
 
       }
+    }
+
+    // update startbutton properties if it still exists 
+    if(this.startButton && !this.startButton.removed) {
+      this.startButton.rotation = this.sbRotate.update();
+      const scale = this.sbScale.update(); // update scale
+      this.startButton.scale = { x: scale, y: scale };
+      this.startButton.stroke = this.sbStroke.updateCol();
     }
 
     camera.off();
